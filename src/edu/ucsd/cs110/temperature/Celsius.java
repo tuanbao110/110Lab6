5d916d46d6a96be4196c573466f8f6957d54ddb7
Tuan Bao <tbao@ucsd.edu>
2017-02-15 12:10:03 -0800

    message

---
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -0,0 +1,26 @@
+package edu.ucsd.cs110.temperature;
+
+public class Celsius extends Temperature
+{
+    public Celsius(float t)
+    {
+        super(t);
+    }
+
+    @Override
+    public Temperature toCelsius() {
+        return this;
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        float val = ((this.getValue() * 9)/5) + 32;
+        return new Fahrenheit(val);
+    }
+
+    public String toString()
+    {
+        // TODO: Complete this method
+        return this.getValue() + " C";
+    }
+}
